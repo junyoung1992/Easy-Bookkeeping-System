4c91115c5dfc713e2ef36b2e276408b40f59a4e8
Kim Junyoung <orsay0827@gmail.com>
2023-08-13 16:58:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Easy Bookkeeping System
+
+간편장부 시스템 구현
+
+## 참고자료
+
+[국세청 - 간편장부 안내](https://www.nts.go.kr/nts/cm/cntnts/cntntsView.do?mi=2231&cntntsId=7670)
---
+++ src/main/java/com/example/easybookkeepingsystem/EasyBookkeepingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.easybookkeepingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBookkeepingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBookkeepingSystemApplication.class, args);
+    }
+
+}
